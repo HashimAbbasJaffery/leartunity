fe997ef18baa20306c39e4c9957c63e92b4a5319
Hashim Abbas JAffery <habbas2121@outlook.com>
2024-10-08 21:42:19 +0500

    minor chnages

--- leartunity/resources/js/Components/Section.vue
+++ leartunity/resources/js/Components/Section.vue
@@ -134,7 +134,7 @@ function addContent(id, action = 1) {
     modal.oneInput("Upload Content", successUpload, true, true, "html", uploadPreparation,
     '<input type="text" id="content-title" class="mb-2" style="width: 100%; border: 1px solid var(--primary); resize: none" ><textarea id="content-description" type="text" style="width: 100%; border: 1px solid var(--primary); height: 100px; resize: none" class="mb-2"></textarea> <input id="content-video" style="border: none;width: 100%;" type="file" value="Choose Files"/>', csrf)
 }
-</script>
+    </script>
 
 <style>
 
