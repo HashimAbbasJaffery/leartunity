5a333aa04af4f498cf23250ffd0621df6ad53f83
Hashim Abbas JAffery <habbas2121@outlook.com>
2024-10-14 18:07:09 +0500

    FileStructure Formatting

--- code_run.bat
+++
@@ -1,2 +0,0 @@
-code .
-php artisan serve
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./assets/css/styles.css"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
